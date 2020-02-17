be919340eb5c514e047ee580a3e90d84b7a53766
BubblesToTheLimit <felix.brilej@googlemail.com>
2020-02-17 21:19:46 +0100

    Add more tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 import pytest
 
+path="tests/testfile.json"
+
 from hr import cli
 
 def test_failure_on_no_arguments():
@@ -7,5 +9,20 @@ def test_failure_on_no_arguments():
     The cli raises an error if no arguments are given.
     """
     parser = cli.create_parser()
-    with pytest.raises(SystemExit):
+    with pytest.raises(SystemError):
         parser.parse([])
+
+def test_success_if_path_is_included():
+    """
+    The cli raises no error if a path is given as an argument.
+    """
+    parser = cli.create_parser()
+    assert parser.parse([path])
+
+def test_export_true_if_flag_set():
+    """
+    The cli correctly sets the export flag if the export flag is set.
+    """
+    parser = cli.create_parser()
+    args = parser.parse(["--export", path])
+    assert args.export
