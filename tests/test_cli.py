763b04550a910d2c9c7e6500e92433f01ebeae37
BubblesToTheLimit <felix.brilej@googlemail.com>
2020-02-17 21:33:37 +0100

    Add parser fixture

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,33 +5,33 @@ path="tests/testfile.json"
 from hr import cli
 
 
-def test_failure_on_no_arguments():
+@pytest.fixture
+def parser():
+    return cli.create_parser()
+
+def test_failure_on_no_arguments(parser):
     """
     The cli raises an error if no arguments are given.
     """
-    parser = cli.create_parser()
     with pytest.raises(SystemError):
         parser.parse([])
 
-def test_success_if_valid_path_is_included():
+def test_success_if_valid_path_is_included(parser):
     """
     The cli raises no error if a valid path is given as an argument.
     """
-    parser = cli.create_parser()
     assert parser.parse([path])
 
-def test_failure_if_invalid_path_is_included():
+def test_failure_if_invalid_path_is_included(parser):
     """
     The cli raises an error if an invalid path is given as an argument.
     """
-    parser = cli.create_parser()
     with pytest.raises(SystemError):
         parser.parse(["/some/wrong/path.json"])
 
-def test_export_true_if_flag_set():
+def test_export_true_if_flag_set(parser):
     """
     The cli correctly sets the export flag if the export flag is set.
     """
-    parser = cli.create_parser()
     args = parser.parse(["--export", path])
     assert args.export
