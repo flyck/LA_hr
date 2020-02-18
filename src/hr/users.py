8a1a41b1832ed4c47eadf7ee1621a1802f637b29
BubblesToTheLimit <felix.brilej@googlemail.com>
2020-02-18 20:02:56 +0100

    Update users, add management

--- src/hr/users.py
+++ src/hr/users.py
@@ -2,26 +2,45 @@ import subprocess
 import pwd, spwd
 
 def delete(user):
+    # Don't touch root!
     if user == "root":
         return
 
     try:
+        print(f"Deleting user {user}.", end = " ")
         subprocess.run(["userdel", user], stdout = subprocess.PIPE)
+        print("Done.")
     except:
-        pass
+        print("Failed!")
 
 def create(user, groups, password):
+    """
+    Creates a user if it doesn't exist.
+    """
+    user_nonexistend = False
     try:
         pwd.getpwnam(user)
     except KeyError:
-        subprocess.run(
-            ["useradd", user, "-G", ",".join(groups), "-p", password], 
-            stdout = subprocess.PIPE
-    )
+        user_nonexistend = True
+
+    if user_nonexistend:
+        try: 
+            print(f"Creating user {user}.", end = " ")
+            subprocess.run(
+                ["useradd", user, "-G", ",".join(groups), "-p", password], 
+                stdout = subprocess.PIPE
+            )
+            print("Done.")
+        except:
+            print("Failed!")
+        return True
+    else:
+        return False
+        
 
 def update(user, groups, password):
     """
-    Update an existing user if necessary
+    Updates an existing user if necessary.
     """
     usermod_params = []
     
@@ -36,5 +55,22 @@ def update(user, groups, password):
         usermod_params += ["-p", password]
 
     if usermod_params:
-        subprocess.run(["usermod", user] + usermod_params, stdout = subprocess.PIPE)
-    
+        try:
+            print(f"Updating user {user}.", end = " ")
+            subprocess.run(["usermod", user] + usermod_params, stdout = subprocess.PIPE)
+            print("Done.")
+        except:
+            print("Failed!")
+
+
+def manage(users):
+    for user in users:
+        try:
+            name = user["name"]
+            groups = user["groups"]
+            password = user ["password"]
+        except KeyError:
+            print(f"Error. User information incomplete. Skipping: {user}")
+        else:
+            create(name, groups, password)
+            update(name, groups, password)
